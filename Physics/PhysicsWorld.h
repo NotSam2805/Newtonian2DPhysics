03d18118a4ed3a4bb2cbb55434edfaf04b79c4d4
Sam Duncliffe <sam.duncliffe@outlook.com>
2026-08-04 20:45:00 +0100

    Refactor Rigidbody and PhysicsWorld classes to use pointers for Rigidbody instances and streamline integration logic

--- Physics/PhysicsWorld.h
+++ Physics/PhysicsWorld.h
@@ -4,6 +4,14 @@
 namespace n2p{
     class PhysicsWorld {
     private:
-        std::vector<Rigidbody> bodies;
+        std::vector<Rigidbody*> bodies;
+    public:
+        void Step(float dt){
+            /* 
+            Performs one time step, moving dt seconds.
+            Apply forces body -> Integrate body
+            Loop through all bodies
+            */
+        }
     };
 }
--- Physics/Rigidbody.h
+++ Physics/Rigidbody.h
@@ -4,26 +4,28 @@ namespace n2p{
     class Rigidbody{
     private:
         float mass;
-        float inertia;
+        float inverseMass;// To avoid expensive division
 
         Transform transform;
         Vector2 velocity;
-        float angularVelocity;
 
         Vector2 accumulatedForces;
-        float accumulatedTorque;
     public:
         // Constructor
         Rigidbody(float mass = 1.0, Transform transform = {Vector2(), 0}, float inertia = 1.0) : 
-        mass(mass), inertia(inertia), transform(transform), velocity(Vector2()), angularVelocity(0), accumulatedForces(Vector2()), accumulatedTorque(0) {}
+        mass(mass), transform(transform), velocity(Vector2()), accumulatedForces(Vector2()), inverseMass(1/mass) {}
 
         void AddForce(Vector2 force){
             accumulatedForces += force;
         }
 
-        void AddTorque(float torque){
-            accumulatedTorque += torque;
-        }
+        void Integrate(float dt){
+            // Semi-implicit Euler
+            Vector2 acceleration = accumulatedForces * inverseMass;
+            velocity += acceleration * dt;
+            transform.position += velocity * dt;
 
+            accumulatedForces = Vector2::Zero();
+        }
     };
 }
