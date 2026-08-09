8e9e58b5f13dd4cad60715199f09acf51696d2cb
Sam Duncliffe <sam.duncliffe@outlook.com>
2026-08-09 20:33:09 +0100

    Implement Clamp and Lerp functions in MathUtils.cpp and update declarations in MathUtils.h

---
+++ Maths/MathUtils.cpp
@@ -0,0 +1,12 @@
+#include "MathUtils.h"
+
+namespace n2p{
+    inline float Clamp(float value, float min, float max){
+        value = (value < min) ? min : value;
+        return (value > max) ? max : value;
+    }
+
+    inline float Lerp(float a, float b, float t){
+        return a + ((b-a) * t);
+    }
+}
--- Maths/MathUtils.h
+++ Maths/MathUtils.h
@@ -1,10 +1,5 @@
 namespace n2p{
-    inline float Clamp(float value, float min, float max){
-        value = (value < min) ? min : value;
-        return (value > max) ? max : value;
-    }
+    inline float Clamp(float value, float min, float max);
 
-    inline float Lerp(float a, float b, float t){
-        return a + ((b-a) * t);
-    }
+    inline float Lerp(float a, float b, float t);
 }
