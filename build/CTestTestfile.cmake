ce7c9550b6fd8b0dc9a48fe07c6c1bec8a9d5acb
Sam Duncliffe <sam.duncliffe@outlook.com>
2026-08-12 18:30:48 +0100

    Revert "Setup CMake project"
    This reverts commit 937660c50b0b738291c1bd3fef942ff9a92e7bbc.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/samdu/OneDrive/ProgrammingStuff/C++/Newtonian2DPhysics
-# Build directory: C:/Users/samdu/OneDrive/ProgrammingStuff/C++/Newtonian2DPhysics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
