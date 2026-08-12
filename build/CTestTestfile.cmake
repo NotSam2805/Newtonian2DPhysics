937660c50b0b738291c1bd3fef942ff9a92e7bbc
Sam Duncliffe <sam.duncliffe@outlook.com>
2026-08-12 16:38:30 +0100

    Setup CMake project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/samdu/OneDrive/ProgrammingStuff/C++/Newtonian2DPhysics
+# Build directory: C:/Users/samdu/OneDrive/ProgrammingStuff/C++/Newtonian2DPhysics/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
